6cfacc16b315139a82499236ca4fecffb39d9b10
Chris Klink <p.c.klink@gmail.com>
2023-02-07 21:43:56 +0100

    wip

--- NMTv2/reg_scripts/SingleSubject_affine_ONPRC18.sh
+++ NMTv2/reg_scripts/SingleSubject_affine_ONPRC18.sh
@@ -32,119 +32,120 @@ TRANSFORM=${AFF_T2S}
 REF=${SS}
 INTERP=Linear
 
-echo Transforming ${IN}
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3 \
-                    -e 2
-ReorientTensorImage 3 ${OUT} ${OUT2} ${TRANSFORM}
+# echo Transforming ${IN}
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3 \
+#                     -e 2
+# ReorientTensorImage 3 ${OUT} ${OUT2} ${TRANSFORM}
 
 # >> CHeck if this works?
-# 3dAllineate \
-#     -source ${IN} \
-#     -prefix ${OUT} \
-#     -master ${REF} \
-#     -1Dmatrix_apply ${TRANSFORM} \
-#     -interp linear -final cubic -overwrite
+3dAllineate \
+    -source ${IN} \
+    -prefix ${OUT} \
+    -master ${REF} \
+    -1Dmatrix_apply ${TRANSFORM} \
+    -interp linear -final cubic -overwrite
+ReorientTensorImage 3 ${OUT} ${OUT2} ${TRANSFORM}
 
 
 
 
-IN=${ONPRC_SUPP}/ONPRC18_DTI_b0_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_b0_in_${SUB}.nii.gz
-echo Transforming ${IN}
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3 \
+# IN=${ONPRC_SUPP}/ONPRC18_DTI_b0_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_b0_in_${SUB}.nii.gz
+# echo Transforming ${IN}
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3 \
 
-IN=${ONPRC_SUPP}/ONPRC18_DTI_rgb_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_rgb_in_${SUB}.nii.gz
-echo Transforming ${IN}
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3 \
+# IN=${ONPRC_SUPP}/ONPRC18_DTI_rgb_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_rgb_in_${SUB}.nii.gz
+# echo Transforming ${IN}
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3 \
 
-IN=${ONPRC_SUPP}/ONPRC18_DTI_fa_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_fa_in_${SUB}.nii.gz
-echo Transforming ${IN}
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3 \
+# IN=${ONPRC_SUPP}/ONPRC18_DTI_fa_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_fa_in_${SUB}.nii.gz
+# echo Transforming ${IN}
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3 \
 
-IN=${ONPRC_SUPP}/ONPRC18_DTI_rd_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_rd_in_${SUB}.nii.gz
-echo Transforming ${IN}
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3 \
+# IN=${ONPRC_SUPP}/ONPRC18_DTI_rd_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_rd_in_${SUB}.nii.gz
+# echo Transforming ${IN}
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3 \
 
-IN=${ONPRC_SUPP}/ONPRC18_DTI_ad_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_ad_in_${SUB}.nii.gz
-echo Transforming ${IN}
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3 \
+# IN=${ONPRC_SUPP}/ONPRC18_DTI_ad_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_ad_in_${SUB}.nii.gz
+# echo Transforming ${IN}
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3 \
 
-IN=${ONPRC_SUPP}/ONPRC18_DTI_tr_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_tr_in_${SUB}.nii.gz
-echo Transforming ${IN}
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3 \
+# IN=${ONPRC_SUPP}/ONPRC18_DTI_tr_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_DTI_tr_in_${SUB}.nii.gz
+# echo Transforming ${IN}
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3 \
 
 
 
-# warp labelmaps ======================================================
-IN=${ONPRC_SUPP}/ONPRC18_GrayMatterLabelmap_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_GrayMatterLabelmap_in_${SUB}.nii.gz
-INTERP=NearestNeighbor
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3
+# # warp labelmaps ======================================================
+# IN=${ONPRC_SUPP}/ONPRC18_GrayMatterLabelmap_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_GrayMatterLabelmap_in_${SUB}.nii.gz
+# INTERP=NearestNeighbor
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3
 
-IN=${ONPRC_SUPP}/ONPRC18_ONPRC18_GrayMatterLabelmapCondensed_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_ONPRC18_GrayMatterLabelmapCondensed_in_${SUB}.nii.gz
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3
+# IN=${ONPRC_SUPP}/ONPRC18_ONPRC18_GrayMatterLabelmapCondensed_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_ONPRC18_GrayMatterLabelmapCondensed_in_${SUB}.nii.gz
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3
 
-IN=${ONPRC_SUPP}/ONPRC18_ONPRC18_GrayMatterWhiteMatterLabelmap_in_NMT_v2.0_sym.nii.gz
-OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_ONPRC18_GrayMatterWhiteMatterLabelmap_in_${SUB}.nii.gz
-antsApplyTransforms -i ${IN} \
-                    -r ${REF} \
-                    -o ${OUT} \
-                    -t ${TRANSFORM} \
-                    -n ${INTERP} \
-                    -d 3
+# IN=${ONPRC_SUPP}/ONPRC18_ONPRC18_GrayMatterWhiteMatterLabelmap_in_NMT_v2.0_sym.nii.gz
+# OUT=${SS_AFF_OUT}/ONPRC18/ONPRC18_ONPRC18_GrayMatterWhiteMatterLabelmap_in_${SUB}.nii.gz
+# antsApplyTransforms -i ${IN} \
+#                     -r ${REF} \
+#                     -o ${OUT} \
+#                     -t ${TRANSFORM} \
+#                     -n ${INTERP} \
+#                     -d 3
 
-# copy the LUTs too
-echo Copying labelmaps
-cp ${ONPRC_SUPP}/*.txt ${SS_AFF_OUT}/ONPRC18/
-cp ${ONPRC_SUPP}/*.tsv ${SS_AFF_OUT}/ONPRC18/
+# # copy the LUTs too
+# echo Copying labelmaps
+# cp ${ONPRC_SUPP}/*.txt ${SS_AFF_OUT}/ONPRC18/
+# cp ${ONPRC_SUPP}/*.tsv ${SS_AFF_OUT}/ONPRC18/
