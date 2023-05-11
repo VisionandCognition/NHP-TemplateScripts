10f20bafe94171dc116a58ef718faaab8038e018
Chris Klink <p.c.klink@gmail.com>
2023-05-11 17:01:49 +0200

    LGN retinotopy pipelines added

---
+++ NMTv2/reg_scripts/SingleSubject_reg_affine_Retinotopy-LGN.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Use the affine registration, e.g. to compensate for head-post drop-out
+
+# =========================
+SUB=$1
+# =========================
+
+BASEFLD=/NHP_MRI/Template/NMT_v2.0/NMT_v2.0_sym
+SSFLD=${BASEFLD}/SingleSubjects
+SCRIPTFLD=${SSFLD}/reg_scripts
+
+# Identify files
+AFF_S2T=${SSFLD}/aligned_${SUB}/${SUB}_composite_linear_to_template.1D
+AFF_T2S=${SSFLD}/aligned_${SUB}/${SUB}_composite_linear_to_template_inv.1D
+
+SS=${SSFLD}/aligned_${SUB}/${SUB}.nii.gz
+
+RETINOTOPY_AFF_OUT=${SSFLD}/aligned_${SUB}/Retinotopy-LGN/affine
+TT=${SSFLD}/aligned_${SUB}/NMT_v2.0_sym.nii.gz
+
+# make folders
+mkdir -p ${RETINOTOPY_AFF_OUT}
+
+declare -a wfld=(
+    LGN-Retmap_in_NMTv2.0_nowarp
+    LGN-Retmap_in_NMTv2.0_aff
+    LGN-Retmap_in_NMTv2.0_nonlin
+    )
+
+declare -a hf=(L R)
+declare -a dt=(
+    mask
+    CELLS
+    ECC
+    INCL
+    )
+declare res=('.nii.gz' '_rsNMT.nii.gz')
+
+for w in "${wfld[@]}"
+do
+    for d in "${dt[@]}"
+    do
+        for h in "${hf[@]}"
+        do
+            for r in "${rs[@]}"
+            do
+                mkdir -p ${RETINOTOPY_AFF_OUT}/${w}
+
+                3dAllineate \
+                    -source ${BASEFLD}/NMT_v2.0_sym/supplemental_RETINOTOPY/LGN/${w}/${d}_${h}${r} \
+                    -prefix ${RETINOTOPY_AFF_OUT}/${w}/${d}_${h}${r} \
+                    -master ${SS} \
+                    -1Dmatrix_apply ${AFF_T2S} \
+                    -interp NN -final NN -overwrite
+            done
+        done
+    done
+done
---
+++ NMTv2/reg_scripts/SingleSubject_reg_nonlinear_Retinotopy-LGN.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Use the nonlinear registration
+
+# =========================
+SUB=$1
+# =========================
+
+BASEFLD=/NHP_MRI/Template/NMT_v2.0/NMT_v2.0_sym
+SSFLD=${BASEFLD}/SingleSubjects
+SCRIPTFLD=${SSFLD}/reg_scripts
+
+# Identify files
+AFF_S2T=${SSFLD}/aligned_${SUB}/${SUB}_composite_linear_to_template.1D
+AFF_T2S=${SSFLD}/aligned_${SUB}/${SUB}_composite_linear_to_template_inv.1D
+NL_S2T=${SSFLD}/aligned_${SUB}/${SUB}_shft_WARP.nii.gz
+NL_T2S=${SSFLD}/aligned_${SUB}/${SUB}_shft_WARPINV.nii.gz
+
+fw_T2S=${SSFLD}/aligned_${SUB}/intermediate/${SUB}_shft_base2osh_WARP.nii.gz
+fwsh_T2S=${SSFLD}/aligned_${SUB}/intermediate/${SUB}_shft_inv.1D
+fw_S2T=${SSFLD}/aligned_${SUB}/intermediate/${SUB}_shft_osh2base_WARP.nii.gz
+fwsh_S2T=${SSFLD}/aligned_${SUB}/intermediate/${SUB}_shft.1D
+
+SS=${SSFLD}/aligned_${SUB}/${SUB}.nii.gz
+RETINOTOPY_NL_OUT=${SSFLD}/aligned_${SUB}/Retinotopy-LGN/nonlinear
+
+TT=${SSFLD}/aligned_${SUB}/NMT_v2.0_sym.nii.gz
+
+# make folders
+mkdir -p ${RETINOTOPY_NL_OUT}
+
+declare -a wfld=(
+    LGN-Retmap_in_NMTv2.0_nowarp
+    LGN-Retmap_in_NMTv2.0_aff
+    LGN-Retmap_in_NMTv2.0_nonlin
+    )
+
+declare -a hf=(L R)
+declare -a dt=(
+    mask
+    CELLS
+    ECC
+    INCL
+    )
+declare res=('.nii.gz' '_rsNMT.nii.gz')
+
+for w in "${wfld[@]}"
+do
+    for d in "${dt[@]}"
+    do
+        for h in "${hf[@]}"
+        do
+            for r in "${rs[@]}"
+            do
+                mkdir -p ${RETINOTOPY_NL_OUT}/${w}
+
+                3dNwarpApply \
+                    -source ${BASEFLD}/NMT_v2.0_sym/supplemental_RETINOTOPY/LGN/${w}/${d}_${h}${r} \
+                    -prefix ${RETINOTOPY_NL_OUT}/${w}/${d}_${h}${r} \
+                    -master ${SS} \
+                    -nwarp  ${fw_T2S} \
+                    -interp NN -overwrite  
+
+                @Align_Centers -overwrite \
+                          -no_cp \
+                          -base ${TT} \
+                          -dset ${RETINOTOPY_NL_OUT}/${w}/${d}_${h}${r} \
+                          -shift_xform_inv ${fwsh_T2S} 
+            done
+        done
+    done
+done
