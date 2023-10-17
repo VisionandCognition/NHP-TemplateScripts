01308f14e26d10d317b0d725375137e497a72388
Chris Klink <p.c.klink@gmail.com>
2023-10-17 20:52:05 +0200

    clean up

--- OLD/create_ss_atlas_NMTv1.3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NMTPATH="/media/DATA1/NHP_MRI/Template/NMT_v1.3"
-
-#./extract_roi_NMTv1.3.sh $1 "${NMTPATH}"
-cp ${NMTPATH}/D99_label* ${NMTPATH}/single_subject_scans/$1/
-./create_xml_NMTv1.3.sh $1 "${NMTPATH}/"
